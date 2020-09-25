3bbe468b508858904221f97b4e4b145b1cbbb9a8
aleksei gurylev <aleksei@gurylev.net>
2020-09-25 22:01:33 +0200

    Revert "Add Discovery Client and Circuit Breaker"
    This reverts commit b9f5b746

--- account-service/src/main/java/net/gurylev/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/net/gurylev/accountservice/AccountServiceApplication.java
@@ -2,12 +2,8 @@ package net.gurylev.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableCircuitBreaker
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
