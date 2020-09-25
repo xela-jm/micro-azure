d33571cc5ec8b2c03808bdea5a30dc03742be835
aleksei gurylev <aleksei@gurylev.net>
2020-09-25 18:27:18 +0200

    .

--- account-service/src/main/java/net/gurylev/accountservice/HelloController.java
+++ account-service/src/main/java/net/gurylev/accountservice/HelloController.java
@@ -7,11 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @Value("${application.message:Not configured by a Spring Cloud Server}")
-    private String message;
-
     @GetMapping("/hello")
     public String hello() {
-        return message + '\n';
+        return "Hello" + '\n';
     }
 }
