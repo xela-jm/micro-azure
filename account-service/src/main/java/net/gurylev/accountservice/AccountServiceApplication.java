b9f5b746eae4a0857071e376cf3cf017d626ab6f
aleksei gurylev <aleksei@gurylev.net>
2020-09-25 21:48:39 +0200

    Add Discovery Client and Circuit Breaker

--- account-service/src/main/java/net/gurylev/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/net/gurylev/accountservice/AccountServiceApplication.java
@@ -2,8 +2,12 @@ package net.gurylev.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
